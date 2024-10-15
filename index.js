e9be87277ab862a03a8c3cd2001da8fb42392557
florence <loracodes3@gmail.com>
2024-10-15 21:51:20 +0300

    array iteration

---
+++ index.js
@@ -0,0 +1,46 @@
+const users = [
+    {
+      firstName: "Niky",
+      lastName: "Morgan",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Jaguar",
+      personalQuote: "You're never too old to learn something new",
+    },
+    {
+      firstName: "Tracy",
+      lastName: "Lum",
+      favoriteColor: "Yellow",
+      favoriteAnimal: "Penguin",
+      personalQuote: "I just got lost in thought - it was unfamiliar territory",
+    },
+    {
+      firstName: "Josh",
+      lastName: "Rowley",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Penguin",
+      personalQuote: "Always remember you’re unique, just like everyone else",
+    },
+    {
+      firstName: "Kate",
+      lastName: "Travers",
+      favoriteColor: "Red",
+      favoriteAnimal: "Jaguar",
+      personalQuote: "Behind every great man is a woman rolling her eyes",
+    },
+    {
+      firstName: "Avidor",
+      lastName: "Turkewitz",
+      favoriteColor: "Blue",
+      favoriteAnimal: "Penguin",
+      personalQuote:
+        "You don’t have to see the whole staircase, just take the first step",
+    },
+    {
+      firstName: "Drew",
+      lastName: "Price",
+      favoriteColor: "Yellow",
+      favoriteAnimal: "Elephant",
+      personalQuote:
+        "Failure is not the opposite of success: it’s part of success",
+    },
+  ];
